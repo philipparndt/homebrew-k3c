23abfbada51fe41f97ca9fbe576c0159fa4b525e
Philipp Arndt <opensource@rnd7.de>
2026-06-15 17:04:50 +0200

    Brew formula update for k3c version v0.12.3

--- Formula/k3c.rb
+++ Formula/k3c.rb
@@ -5,13 +5,13 @@
 class K3c < Formula
   desc "Local k3s clusters on Apple container — like k3d, without Docker"
   homepage "https://github.com/philipparndt/k3c"
-  version "0.12.2"
+  version "0.12.3"
   license "Apache-2.0"
   depends_on :macos
 
   if Hardware::CPU.arm?
-    url "https://github.com/philipparndt/k3c/releases/download/v0.12.2/k3c_darwin_arm64.tar.gz"
-    sha256 "c317ec9954dab79fd70c0ac72ba0fd4671811f315a7adff0937887324d381c69"
+    url "https://github.com/philipparndt/k3c/releases/download/v0.12.3/k3c_darwin_arm64.tar.gz"
+    sha256 "874908af0370e1d1cfa10b2645a1653dda8e02e84a70cc2ad75582fae593e8d1"
 
     define_method(:install) do
       bin.install "k3c"
